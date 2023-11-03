dcfea5fcdd96a35bf19b4804315d41af2e801ccd
HRileyTang <hrileytang@gmail.com>
2023-11-03 11:24:40 -0700

    rename, semmmiiioaicelon

--- arduinoScreen/arduinoScreen.ino
+++ arduinoScreen/arduinoScreen.ino
@@ -1,5 +1,5 @@
 // Import the Liquid Crystal library
-#include <LiquidCrystal.h>;
+#include <LiquidCrystal.h>
 //Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
