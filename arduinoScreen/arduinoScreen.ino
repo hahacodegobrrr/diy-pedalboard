b4cb71e1959f962c496f69da390d0b3c789ca961
Ethan Reeves <148184422+ethanhreeves@users.noreply.github.com>
2023-11-03 11:46:58 -0700

    link to wire

--- arduinoScreen/arduinoScreen.ino
+++ arduinoScreen/arduinoScreen.ino
@@ -1,4 +1,6 @@
 // Import the Liquid Crystal library
+//https://docs.arduino.cc/learn/electronics/lcd-displays
+//^link used to wire LCD Screen
 #include <LiquidCrystal.h>;
 //Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
