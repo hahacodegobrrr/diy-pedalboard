7f06832526ac32161aa709ed85b355352c8204d9
HRileyTang <hrileytang@gmail.com>
2023-11-22 09:31:06 -0800

    did the tuner note thingymabob

--- TunerUtils.c
+++ TunerUtils.c
@@ -2,16 +2,28 @@
 #include <stdint.h>
 #include <math.h>
 
-float noteTable[] = {
-
-};
-
 const float ln2 = 0.69314718f;
 const float A4 = 440;
 
+struct TunerNote tunerNote;
+
 struct TunerNote* getNote(uint16_t frequency) {
-  float freqTo12Tone = (float)(fmod((log((double)frequency / (double)A4) / ln2), 1));
-  return NULL;
+  float freqTo12Tone = (float)(fmod(log(frequency / A4) / ln2, 1)) * 12;
+  
+  float error = fmod(freqTo12Tone, 1);
+  uint8_t note;
+  if (error >= 0.5) {
+    error -= 1; //wrap around to next note's error
+    note = ((uint8_t)freqTo12Tone + 1) % 12;
+  } else {
+    note = (uint8_t)freqTo12Tone;
+  }
+
+  tunerNote.note = note;
+  tunerNote.error = error;
+
+  //theoretically stays the same, but fuck it
+  return &tunerNote;
 }
 
 uint8_t* generateDisplayMessage() {
--- TunerUtils.h
+++ TunerUtils.h
@@ -7,6 +7,13 @@ struct TunerNote {
   float error;
 };
 
+/**
+ * @brief Takes a frequency and returns a number from 0 to 12(exclusive)
+ * indicating the note (0 is A, 1 is A#, and so on)
+ * 
+ * @param frequency in hertz
+ * @return struct TunerNote* pointer to the tunernote struct
+ */
 struct TunerNote* getNote(uint16_t frequency);
 
 uint8_t* generateDisplayMessage();
