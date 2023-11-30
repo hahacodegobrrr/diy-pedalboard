fbdaa84ffd1bf67f8ff944e832eb67e2e6466780
HRileyTang <hrileytang@gmail.com>
2023-11-30 13:59:08 -0800

    good audio hahahehehahehah

---
+++ Delay/Delay.ino
@@ -0,0 +1,57 @@
+// Delay demonstration example, Teensy Audio Library
+//   http://www.pjrc.com/teensy/td_libs_Audio.html
+// 
+// Creates a chirp on the left channel, then
+// three delayed copies on the right channel.
+//
+// Requires the audio shield:
+//   http://www.pjrc.com/store/teensy3_audio.html
+//
+// This example code is in the public domain.
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+const int input = AUDIO_INPUT_LINEIN;
+
+// GUItool: begin automatically generated code
+AudioInputI2S            audioInput;
+AudioEffectEnvelope      envelope1;      //xy=232,156
+AudioEffectDelay         delay1;         //xy=393,238
+AudioMixer4              mixer1;         //xy=532,205
+AudioOutputI2S           i2s1;           //xy=611,61
+AudioConnection          patchCord1(audioInput, envelope1);
+AudioConnection          patchCord2(envelope1, delay1);
+AudioConnection          patchCord3(envelope1, 0, i2s1, 0);
+AudioConnection          patchCord4(delay1, 0, mixer1, 0);
+AudioConnection          patchCord5(delay1, 1, mixer1, 1);
+AudioConnection          patchCord6(delay1, 2, mixer1, 2);
+AudioConnection          patchCord7(audioInput, 0, mixer1, 3);
+AudioConnection          patchCord8(mixer1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=195,272
+// GUItool: end automatically generated code
+
+void setup() {
+  // allocate enough memory for the delay
+  AudioMemory(120);
+  
+  // enable the audio shield
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(input);
+  sgtl5000_1.volume(0.5);
+  
+
+  // create 3 delay taps, which connect through a
+  // mixer to the right channel output
+  delay1.delay(1, 500);
+}
+
+void loop() {
+  envelope1.noteOn();
+  delay(500);
+  envelope1.noteOff();
+  delay(50);
+}
--- diy-pedalboard.ino
+++ diy-pedalboard.ino
@@ -4,9 +4,15 @@
 
 #include <stdint.h>
 
-#include "TunerUtils.h"
-#include "FSM.h"
-
+// #include "TunerUtils.h"
+// #include "FSM.h"
+struct State {
+  uint8_t id;
+  uint16_t knob1;
+  uint16_t knob2;
+  uint16_t knob3;
+  struct State* nextState;
+};
 
 const uint8_t MESSAGE_SIZE = 32; //in bytes
 const uint16_t SCREEN_UPDATE_PERIOD = 1000; //in ms
@@ -50,7 +56,6 @@ void setup(){
 
 void loop(){
   uint64_t now = millis();
-
   //screen
   if (lastScreenUpdate + SCREEN_UPDATE_PERIOD <= now) {
     if (currentState->id == 0 && fft.available()) { //tuner mode
@@ -59,13 +64,18 @@ void loop(){
       uint16_t i;
       for (i = 0; i < 40; i++) {
         float bin = fft.read(i);
+        Serial.print(bin);
+        Serial.print("-");
+
         if (bin > loudest) {
           loudest = bin;
           loudestBin = i;
         }
       }
+      Serial.println();
       //could try linear interpolation here or smth
-      sendMessageToScreen(generateTunerDisplayMessage(loudestBin * FFT_BIN_SIZE), MESSAGE_SIZE);
+      uint8_t* msg = generateTunerDisplayMessage(loudestBin * FFT_BIN_SIZE);
+      sendMessageToScreen(msg, MESSAGE_SIZE);
     }
   }
 }
@@ -77,4 +87,76 @@ void loop(){
 void sendMessageToScreen(uint8_t* p_text, uint8_t length) {
   Serial1.write(0); //signal beginning of message
   Serial1.write(p_text, length);
+}
+
+
+//from tunerutils
+
+#include <math.h>
+
+#define MESSAGE_SIZE 32
+
+struct TunerNote {
+  uint8_t note;
+  float error;
+};
+
+const float ln2 = 0.69314718f;
+const float FREQ_A4 = 440;
+
+uint8_t noteTable[] = {'A','a','B','C','c','D','d','E','F','f','G','g'};
+
+struct TunerNote tunerNote;
+uint8_t tunerMessage[MESSAGE_SIZE] = {'T','U','N','E','R',' ',' ',' ','N','O','T','E',':',' '};
+
+void getNote(uint16_t frequency) {
+  float freqTo12Tone = (float)(fmod(log(frequency / FREQ_A4) / ln2, 1)) * 12;
+  
+  float error = fmod(freqTo12Tone, 1);
+  uint8_t note;
+  if (error >= 0.5) {
+    error -= 1; //wrap around to next note's error
+    note = ((uint8_t)freqTo12Tone + 1) % 12;
+  } else {
+    note = (uint8_t)freqTo12Tone;
+  }
+
+  tunerNote.note = note;
+  tunerNote.error = error;
+}
+
+uint8_t* generateTunerDisplayMessage(uint16_t frequency) {
+  //message indices 0-13 have already been written to
+  getNote(frequency);
+  if (noteTable[tunerNote.note] >= 'a') {
+    tunerMessage[14] = noteTable[tunerNote.note] - 32;
+    tunerMessage[15] = '#';
+  } else {
+    tunerMessage[14] = noteTable[tunerNote.note];
+    tunerMessage[15] = ' ';
+  }
+  int i;
+  for (i = 0; i < MESSAGE_SIZE / 2; i++) {
+    float transI = (i - 7.5) * 0.5 / 7.5; //transforms (0-15) to (-0.5-0.5)
+    tunerMessage[i + MESSAGE_SIZE / 2] = (tunerNote.error > 0)? (transI < tunerNote.error && transI > 0)? '-' : ' ' : (transI > tunerNote.error && transI < 0)? '-' : ' ';
+  }
+
+  return &tunerMessage[0];
+}
+
+//from fsm
+//tuner, chorus, delay, freeverb
+
+
+struct State stateTable[] = {
+  {.id = 0, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[1]},
+  {.id = 1, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[2]},
+  {.id = 2, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[3]},
+  {.id = 3, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[0]}
+};
+
+struct State* getNextState(struct State* currentState) {
+  if (currentState == 0)
+    return &stateTable[0];
+  return (*currentState).nextState;
 }
