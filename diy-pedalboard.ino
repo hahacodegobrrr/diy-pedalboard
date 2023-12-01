77522f8aea3b75621d198c83b97eb1ac1466b9d5
HRileyTang <hrileytang@gmail.com>
2023-11-30 18:06:26 -0800

    condensed files into diy-pedalboard.ino

--- diy-pedalboard.ino
+++ diy-pedalboard.ino
@@ -1,11 +1,24 @@
-#include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
+//arduino libraries
+#include <Audio.h> //for processing audio
+#include <Wire.h> //communication protocol (i2c)
+#include <SPI.h> //communication protocol
 
-#include <stdint.h>
+//c libraries
+#include <stdint.h> //extended int types
+#include <math.h> //extended math operations
 
-// #include "TunerUtils.h"
-// #include "FSM.h"
+//used for tuner
+struct TunerNote {
+  uint8_t note;
+  float error;
+};
+const float ln2 = 0.69314718f;
+const float FREQ_A4 = 440;
+uint8_t noteTable[] = {'A','a','B','C','c','D','d','E','F','f','G','g'};
+struct TunerNote tunerNote;
+uint8_t tunerMessage[MESSAGE_SIZE] = {'T','U','N','E','R',' ',' ',' ','N','O','T','E',':',' '};
+
+//used for FSM
 struct State {
   uint8_t id;
   uint16_t knob1;
@@ -15,13 +28,14 @@ struct State {
 };
 
 const uint8_t MESSAGE_SIZE = 32; //in bytes
-const uint16_t SCREEN_UPDATE_PERIOD = 1000; //in ms
+const uint16_t SCREEN_UPDATE_PERIOD = 100; //in ms
 const float FFT_BIN_SIZE = 43; //hz
 
 //switch depending on which one we're using
 const uint8_t input = AUDIO_INPUT_LINEIN;
 // const uint8_t input = AUDIO_INPUT_MIC;
 
+//for tracking timing
 uint64_t lastScreenUpdate;
 
 //audio shit
@@ -34,10 +48,11 @@ AudioAnalyzeFFT1024 fft;
 //a module cannot have multiple inputs -> send to a mixer
 //a module can have multiple outputs
 AudioConnection patchCord1(audioInput, 0, fft, 0);
-AudioConnection patchCord2(fft, 0, audioOutput, 0);
+AudioConnection patchCord2(audioInput, 0, audioOutput, 0);
 
 struct State* currentState;
 
+//buffer for message to be displayed on screen
 uint8_t screenMessage[MESSAGE_SIZE];
 
 void setup(){
@@ -57,22 +72,19 @@ void setup(){
 void loop(){
   uint64_t now = millis();
   //screen
-  if (lastScreenUpdate + SCREEN_UPDATE_PERIOD <= now) {
+  if (lastScreenUpdate + SCREEN_UPDATE_PERIOD >= now) {
     if (currentState->id == 0 && fft.available()) { //tuner mode
       uint16_t loudestBin = 0;
       float loudest = 0;
       uint16_t i;
       for (i = 0; i < 40; i++) {
         float bin = fft.read(i);
-        Serial.print(bin);
-        Serial.print("-");
 
         if (bin > loudest) {
           loudest = bin;
           loudestBin = i;
         }
       }
-      Serial.println();
       //could try linear interpolation here or smth
       uint8_t* msg = generateTunerDisplayMessage(loudestBin * FFT_BIN_SIZE);
       sendMessageToScreen(msg, MESSAGE_SIZE);
@@ -89,26 +101,10 @@ void sendMessageToScreen(uint8_t* p_text, uint8_t length) {
   Serial1.write(p_text, length);
 }
 
-
-//from tunerutils
-
-#include <math.h>
-
-#define MESSAGE_SIZE 32
-
-struct TunerNote {
-  uint8_t note;
-  float error;
-};
-
-const float ln2 = 0.69314718f;
-const float FREQ_A4 = 440;
-
-uint8_t noteTable[] = {'A','a','B','C','c','D','d','E','F','f','G','g'};
-
-struct TunerNote tunerNote;
-uint8_t tunerMessage[MESSAGE_SIZE] = {'T','U','N','E','R',' ',' ',' ','N','O','T','E',':',' '};
-
+//TUNER
+/** Converts a frequency into the corresponding note
+ * frequency -> frequency, in hertz
+ */
 void getNote(uint16_t frequency) {
   float freqTo12Tone = (float)(fmod(log(frequency / FREQ_A4) / ln2, 1)) * 12;
   
@@ -125,6 +121,10 @@ void getNote(uint16_t frequency) {
   tunerNote.error = error;
 }
 
+/** Generates a screen message for the tuner based on a frequency
+ * frequency -> frequency, in hertz
+ * returns a pointer to the message
+ */
 uint8_t* generateTunerDisplayMessage(uint16_t frequency) {
   //message indices 0-13 have already been written to
   getNote(frequency);
@@ -144,10 +144,8 @@ uint8_t* generateTunerDisplayMessage(uint16_t frequency) {
   return &tunerMessage[0];
 }
 
-//from fsm
+//FSM
 //tuner, chorus, delay, freeverb
-
-
 struct State stateTable[] = {
   {.id = 0, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[1]},
   {.id = 1, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[2]},
@@ -155,6 +153,10 @@ struct State stateTable[] = {
   {.id = 3, .knob1 = 0, .knob2 = 0, .knob3 = 0, .nextState = &stateTable[0]}
 };
 
+/** Get the next state in the FSM
+ * currentState -> pointer to the current state
+ * returns a pointer to the next state
+ */
 struct State* getNextState(struct State* currentState) {
   if (currentState == 0)
     return &stateTable[0];
